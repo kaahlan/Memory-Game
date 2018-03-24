3541a41c1ab6043d0cb64737f06d662c664f8811
kaahlan <37257811+kaahlan@users.noreply.github.com>
2018-03-24 01:19:31 -0600

    Feat: Add Base Game Functionality
    Add base game functionality with simple win modal,  show card function and checkMatch function now only let you turn over 2 cards at a time, checks for a match, and flips over if not a match or stays turned up if matched. Added a click disable with a delay to prevent clicking for 1 second after 2 cards are turned so that player cannot turn up more than 2 at a time. Once all 16 cards are match, basic win modal pops up.
    
    HTML/CSS - Modal and formatting updates.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# memory-game
-Memory Game Project for Udacity IPND
++# memory-game
++Memory Game Project for Udacity IPND
+
++#resources
++Modal codes from w3schools -- https://www.w3schools.com/howto/howto_css_modals.asp.
--- js/app.js
+++ js/app.js
@@ -1,53 +1,91 @@
-/*
- * Create a list that holds all of your cards
- */
-var cards = ["diamond", "diamond", "paper-plane-o", "paper-plane-o", "anchor", "anchor",
-            "bolt", "bolt", "cube", "cube", "leaf", "leaf", "bicycle", "bicycle", "bomb", "bomb"];
+//Global varibales, including List of cards to populate the game deck.
+const cards = ["diamond", "diamond", "paper-plane-o", "paper-plane-o", "anchor", "anchor",
+  "bolt", "bolt", "cube", "cube", "leaf", "leaf", "bicycle", "bicycle", "bomb", "bomb"
+];
+let openCards = [];
+let moves = 0;
+let matches = 0;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+//Creates the game deck using shuffle function and cards array.
 function createDeck() {
- deck = shuffle(cards);
- deck.forEach(function(card) {
-   $('.deck').append('<li class="card"><i class="fa fa-' + card + '"></i></li>');
- })
- openCard();
-};
+  let deck = shuffle(cards);
+  deck.forEach(function(card) {
+    $('.deck').append('<li class="card"><i class="fa fa-' + card + '"></i></li>');
+  });
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
-function openCard() {
+//On click function for cards, flips if not open and then calls match function.
+function showCard() {
   $(".card").on("click", function() {
+    if ($(this).hasClass("open show")) {
+      return;
+    }
     $(this).toggleClass("flipInY open show");
-  })
-};
-
-createDeck();
+    openCards.push($(this));
+    checkMatch();
+  });
+}
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+Matching function, checks once 2 cards are opened. Prevents further clicks on unopened
+cards. Matches stay open, non-matches turn back over, calls win function once all matched.
+*/
+function checkMatch() {
+  if (openCards.length === 2) {
+    moves++;
+    $(".card").off("click");
+    if (openCards[0].children().attr("class") === openCards[1].children().attr("class")) {
+      openCards.forEach(function(card) {
+        card.addClass("match");
+      });
+      setTimeout(function() {
+        matches++;
+        if (matches === 8) {
+          return win();
+        }
+        openCards = [];
+        return showCard();
+      }, 1000);
+    } else {
+      setTimeout(function() {
+        openCards.forEach(function(card) {
+          card.toggleClass("open show");
+        });
+        openCards = [];
+        return showCard();
+      }, 1000);
+    }
+  }
+}
+
+//TODO: Moves function, timer function, reset function, star rating function.
+
+/*
+Basic win functionality.
+TODO: Add time, star score, moves, replay button.
+*/
+function win() {
+  let modal = document.getElementById('myModal');
+
+  modal.style.display = "block";
+}
+
+//Event listeners.
+createDeck();
+showCard();
